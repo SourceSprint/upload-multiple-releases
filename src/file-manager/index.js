e854e8cfab3b8aaab8835307464b64af53ee2b32
David Ibia <david.ibia@boxmarshall.com>
2020-11-18 02:31:27 +0100

    feat: created file manager

---
+++ src/file-manager/index.js
@@ -0,0 +1,22 @@
+const glob = require('glob')
+
+class FileManager {
+  resolveFiles(filelist) {
+    const paths = `${filelist}`.split('\n').filter((line) => line.trim().length)
+
+    const files = paths.map((filePath) => {
+      // Use glob to parse paths with wildcards
+      if (filePath.indexOf('*') !== -1) {
+        return glob.sync(filePath)
+      }
+
+      return filePath
+    })
+
+    return files
+  }
+}
+
+module.exports = {
+  FileManager
+}
